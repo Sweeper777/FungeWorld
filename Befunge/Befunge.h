1604b88d1b573bf298230407617cebf5cef056f3
Mulang Su <sumulang@gmail.com>
2021-06-05 17:58:02 +0800

    added a framework target for the model

---
+++ Befunge/Befunge.h
@@ -0,0 +1,18 @@
+//
+//  Befunge.h
+//  Befunge
+//
+//  Created by Mulang Su on 5/6/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Befunge.
+FOUNDATION_EXPORT double BefungeVersionNumber;
+
+//! Project version string for Befunge.
+FOUNDATION_EXPORT const unsigned char BefungeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Befunge/PublicHeader.h>
+
+
