91d2d89dbc952bd14f3861b3508ffd2f6907ed76
Mulang Su <sumulang@gmail.com>
2021-06-12 12:42:32 +0800

    tried debugging the hello world program in the VC

--- FungeWorld/ViewController.swift
+++ FungeWorld/ViewController.swift
@@ -1,19 +1,56 @@
-//
-//  ViewController.swift
-//  FungeWorld
-//
-//  Created by Mulang Su on 5/6/2021.
-//
-
 import UIKit
+import Befunge
 
-class ViewController: UIViewController {
+class TestIO: IOProtocol {
+    let numberSupplier: () -> Int
+    let charSupplier: () -> UnicodeScalar
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var outputBuffer = ""
+
+    init(numberSupplier: @escaping () -> Int = { fatalError() },
+         charSupplier: @escaping () -> UnicodeScalar = { fatalError() }) {
+        self.numberSupplier = numberSupplier
+        self.charSupplier = charSupplier
+    }
+
+    func writeChar(_ char: UnicodeScalar) {
+        outputBuffer += "\(char)"
+    }
+
+    func writeInt(_ int: Int) {
+        outputBuffer += "\(int) "
+    }
+
+    func writeError(_ message: String) {
+        print("Error: \(message)")
+    }
+
+    func readNumber(completion: @escaping (Int) -> Void) {
+        completion(numberSupplier())
+    }
+
+    func readChar(completion: @escaping (UnicodeScalar) -> Void) {
+        completion(charSupplier())
     }
 
 
+}
+
+class ViewController: UIViewController {
+    let io = TestIO()
+    lazy var state = State(io: io, code: "64+\"!dlroW ,olleH\">:#,_@")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(io.outputBuffer)
+        printPlayfield(state.playfield)
+    }
+
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        state.nextStep {
+            print("Instruction Pointer: \(self.state.instructionPointer)")
+            print("Stack: \(self.state.stack)")
+            print("String Mode: \(self.state.stringMode)")
+        }
+    }
 }
 
