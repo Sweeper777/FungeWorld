19d55f831afe2226424bae62ddded39b7bd17737
Mulang Su <sumulang@gmail.com>
2021-06-16 10:00:17 +0800

    added scene view

--- FungeWorld/ViewController.swift
+++ FungeWorld/ViewController.swift
@@ -1,7 +1,10 @@
 import UIKit
 import Befunge
+import SceneKit
 
 class ViewController: UIViewController {
+    @IBOutlet var sceneView: SCNView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
