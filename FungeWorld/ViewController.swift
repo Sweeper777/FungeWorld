01e0847a074a89e605e035f6becd23e84e512b5e
Mulang Su <sumulang@gmail.com>
2021-06-16 09:59:32 +0800

    removed the bit of messing around that I did

--- FungeWorld/ViewController.swift
+++ FungeWorld/ViewController.swift
@@ -1,56 +1,9 @@
 import UIKit
 import Befunge
 
-class TestIO: IOProtocol {
-    let numberSupplier: () -> Int
-    let charSupplier: () -> UnicodeScalar
-
-    var outputBuffer = ""
-
-    init(numberSupplier: @escaping () -> Int = { fatalError() },
-         charSupplier: @escaping () -> UnicodeScalar = { fatalError() }) {
-        self.numberSupplier = numberSupplier
-        self.charSupplier = charSupplier
-    }
-
-    func writeChar(_ char: UnicodeScalar) {
-        outputBuffer += "\(char)"
-    }
-
-    func writeInt(_ int: Int) {
-        outputBuffer += "\(int) "
-    }
-
-    func writeError(_ message: String) {
-        print("Error: \(message)")
-    }
-
-    func readNumber(completion: @escaping (Int) -> Void) {
-        completion(numberSupplier())
-    }
-
-    func readChar(completion: @escaping (UnicodeScalar) -> Void) {
-        completion(charSupplier())
-    }
-
-
-}
-
 class ViewController: UIViewController {
-    let io = TestIO()
-    lazy var state = State(io: io, code: "64+\"!dlroW ,olleH\">:#,_@")
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(io.outputBuffer)
-        printPlayfield(state.playfield)
-    }
-
-    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        state.nextStep {
-            print("Instruction Pointer: \(self.state.instructionPointer)")
-            print("Stack: \(self.state.stack)")
-            print("String Mode: \(self.state.stringMode)")
-        }
     }
 }
 
