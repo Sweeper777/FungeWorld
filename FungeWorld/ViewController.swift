528fd15addcfc75466ed0f7faa075afa83f5396e
Mulang Su <sumulang@gmail.com>
2021-06-17 10:30:41 +0800

    set up scene in VC

--- FungeWorld/ViewController.swift
+++ FungeWorld/ViewController.swift
@@ -5,8 +5,14 @@ import SceneKit
 class ViewController: UIViewController {
     @IBOutlet var sceneView: SCNView!
 
+    var scene: FungeWorldScene!
     override func viewDidLoad() {
         super.viewDidLoad()
+        scene = FungeWorldScene()
+        scene.setup()
+        sceneView.scene = scene
+        sceneView.pointOfView = scene.cameraNode
+        sceneView.allowsCameraControl = true
     }
 }
 
