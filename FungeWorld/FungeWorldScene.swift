edd91a7b82e2e03520728448528c163c3a58d43b
Mulang Su <sumulang@gmail.com>
2021-06-17 10:29:55 +0800

    added setupCamera

--- FungeWorld/FungeWorldScene.swift
+++ FungeWorld/FungeWorldScene.swift
@@ -2,4 +2,13 @@ import SceneKit
 
 class FungeWorldScene : SCNScene {
     var cameraNode: SCNNode!
+
+    private func setupCamera() {
+        cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position.y = 10
+        cameraNode.eulerAngles.x = -0.523599
+        cameraNode.eulerAngles.y = -.pi / 2
+    }
+
 }
