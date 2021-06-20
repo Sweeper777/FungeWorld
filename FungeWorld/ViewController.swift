31c0b6c4dda3987bca605848f6114a0a45fa4c79
Mulang Su <sumulang@gmail.com>
2021-06-20 11:45:53 +0800

    created State in VC and assigned to scene

--- FungeWorld/ViewController.swift
+++ FungeWorld/ViewController.swift
@@ -4,15 +4,16 @@ import SceneKit
 
 class ViewController: UIViewController {
     @IBOutlet var sceneView: SCNView!
+    lazy var state = State(io: scene, code: "64+\"!dlroW ,olleH\">:#,_@")
 
     var scene: FungeWorldScene!
     override func viewDidLoad() {
         super.viewDidLoad()
         scene = FungeWorldScene()
+        scene.state = state
         scene.setup()
         sceneView.scene = scene
         sceneView.pointOfView = scene.cameraNode
         sceneView.allowsCameraControl = true
     }
 }
-
