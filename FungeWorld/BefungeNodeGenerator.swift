403eeec92af56cadde7b62dc72f274c767f65508
Mulang Su <sumulang@gmail.com>
2021-06-20 11:42:51 +0800

    Create BefungeNodeGenerator.swift

---
+++ FungeWorld/BefungeNodeGenerator.swift
@@ -0,0 +1,15 @@
+import SceneKit
+import SwiftyUtils
+
+enum BefungeNodeGenerator {
+    private static var textureCache: [String: UIImage] = [:]
+
+    static let background = UIColor.black
+    static let foreground = UIColor.white
+
+
+    static let befungeNodeSize = 0.9.f
+    private static let befungeNodeHeight = 0.5.f
+
+}
+
