e98281614533d7ffcf7ae6fe7a7ec380fc4f6bd3
Mulang Su <sumulang@gmail.com>
2021-06-21 10:22:18 +0800

    tried animating camera

--- FungeWorld/ViewController.swift
+++ FungeWorld/ViewController.swift
@@ -17,4 +17,15 @@ class ViewController: UIViewController {
         sceneView.allowsCameraControl = true
         sceneView.defaultCameraController.interactionMode = .pan
     }
+
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        super.motionBegan(motion, with: event)
+        SCNTransaction.begin()
+        SCNTransaction.animationDuration = 0.5
+        sceneView.pointOfView!.eulerAngles.x = 0
+        sceneView.pointOfView?.position.x = 0
+        sceneView.pointOfView?.position.z = 0
+        SCNTransaction.commit()
+        SCNTransaction.animationDuration = 0
+    }
 }
