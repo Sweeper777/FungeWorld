c9bbd9321f9633a3ec6f195e3aad00d138f5eab9
Mulang Su <sumulang@gmail.com>
2021-12-27 09:35:56 +0000

    stopped manually creating the label node

--- FungeWorld/HUDScene.swift
+++ FungeWorld/HUDScene.swift
@@ -5,10 +5,7 @@ class HUDScene: SKScene {
     private var textNode: SKLabelNode!
     
     func setup() {
-        textNode = SKLabelNode(text: "Stack:")
-        textNode.fontSize = 30
-        textNode.fontColor = .black
-        self.addChild(textNode)
+        textNode = childNode(withName: "label") as! SKLabelNode
     }
     
     func updateWithState(_ state: State) {
