5b9e9368ae54dfa23bca7bd1543d762b6a5d941e
Mulang Su <sumulang@gmail.com>
2021-12-28 13:22:35 +0000

    set up collection view layout

--- FungeWorld/BefungeStackViewController.swift
+++ FungeWorld/BefungeStackViewController.swift
@@ -19,4 +19,23 @@ class BefungeStackViewController: UIViewController {
     private lazy var dataSource = makeDataSource()
     
     let stackItemHeight = CGFloat(44)
+    
+    private func makeLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                             heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+//        item.edgeSpacing = .init(leading: .fixed(8), top: .fixed(8), trailing: .fixed(8), bottom: .fixed(8))
+        item.contentInsets = .init(top: 8, leading: 8, bottom: 8, trailing: 8)
+      
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .absolute(stackItemHeight))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                         subitems: [item])
+      
+        let section = NSCollectionLayoutSection(group: group)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+    
 }
