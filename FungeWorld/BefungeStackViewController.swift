c924076f87f3ed90a04b100529d9c2def2426787
Mulang Su <sumulang@gmail.com>
2022-01-02 10:10:10 +0000

    removed the dummy initial stack for testing

--- FungeWorld/BefungeStackViewController.swift
+++ FungeWorld/BefungeStackViewController.swift
@@ -30,9 +30,6 @@ class BefungeStackViewController: UIViewController {
         collectionView.collectionViewLayout = makeLayout()
         collectionView.dataSource = dataSource
         collectionView.transform = .init(scaleX: 1, y: -1)
-        _ = Task {
-            await animateStack([1,2,3,4,5,6,7,8,9,10,11])
-        }
     }
     
     private func makeLayout() -> UICollectionViewLayout {
