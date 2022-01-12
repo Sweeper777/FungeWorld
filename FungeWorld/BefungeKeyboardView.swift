87f0d4607648d75bfe6c46a9e9f6eab38cd0029c
Mulang Su <sumulang@gmail.com>
2022-01-12 22:05:19 +0000

    set up the keys' collection view

--- FungeWorld/BefungeKeyboardView.swift
+++ FungeWorld/BefungeKeyboardView.swift
@@ -14,10 +14,19 @@ private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, String>
 class BefungeKeyboardView: UIView {
     weak var textInput: UITextInput!
     @IBOutlet var collectionView: UICollectionView!
+    private lazy var dataSource = makeDataSource()
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        collectionView.register(UINib(nibName: "BefungeKeyCell", bundle: nil), forCellWithReuseIdentifier: "cell")
+
+        collectionView.collectionViewLayout = makeLayout()
+        collectionView.dataSource = dataSource
+        fillCollectionView()
+    }
 }
 
