02de0e2d79ab8683234f1ce9cce66588703570a6
Mulang Su <sumulang@gmail.com>
2022-01-12 22:04:54 +0000

    added sections for the befunge keyboard

--- FungeWorld/BefungeKeyboardView.swift
+++ FungeWorld/BefungeKeyboardView.swift
@@ -1,5 +1,15 @@
 import UIKit
 
+private enum Section: CaseIterable {
+    case numbers
+    case arithmeticAndLogic
+    case directions
+    case stack
+    case io
+}
+
+private typealias DataSource = UICollectionViewDiffableDataSource<Section, String>
+private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, String>
 
 class BefungeKeyboardView: UIView {
     weak var textInput: UITextInput!
