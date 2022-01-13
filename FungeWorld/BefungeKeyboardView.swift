57a83e339ba1565309d2bbec150070beaaff1be0
Mulang Su <sumulang@gmail.com>
2022-01-13 22:30:57 +0000

    added makeNumbersSection

--- FungeWorld/BefungeKeyboardView.swift
+++ FungeWorld/BefungeKeyboardView.swift
@@ -50,6 +50,36 @@ class BefungeKeyboardView: UIView {
         dataSource.apply(snapshot)
     }
     
+    private func makeLayout() -> UICollectionViewLayout {
+        
+        let contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+        let sectionInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0)
+        
+        func makeNumbersSection() -> NSCollectionLayoutSection {
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/3),
+                                                 heightDimension: .fractionalHeight(1))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let rowGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                      heightDimension: .fractionalHeight(1/4))
+            let zeroKeyItem = NSCollectionLayoutItem(layoutSize: rowGroupSize)
+            item.contentInsets = contentInsets
+            zeroKeyItem.contentInsets = contentInsets
+          
+            let rowGroup = NSCollectionLayoutGroup.horizontal(layoutSize: rowGroupSize,
+                                                             subitem: item, count: 3)
+            let sectionGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: .init(widthDimension: .absolute(150),
+                                  heightDimension: .fractionalHeight(1)),
+                subitems: [rowGroup, rowGroup, rowGroup, zeroKeyItem])
+            
+            let section = NSCollectionLayoutSection(group: sectionGroup)
+            section.contentInsets = sectionInsets
+            return section
+        }
+        
+    }
+    
     private func makeDataSource() -> DataSource {
         let dataSource = DataSource(
             collectionView: collectionView,
