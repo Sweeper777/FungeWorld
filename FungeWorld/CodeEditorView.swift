f487b4e719b9c83624ab932126ce7d8b2bc094e3
Mulang Su <sumulang@gmail.com>
2022-01-25 22:18:03 +0000

    set up toolbar in code editor view

--- FungeWorld/CodeEditorView.swift
+++ FungeWorld/CodeEditorView.swift
@@ -33,6 +33,20 @@ class CodeEditorView: UIScrollView {
         setupScrollView()
     }
     func setupTextView() {
+        toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 100, height: 44))
+        toolBar.items = [
+            UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(hideKeyboard)),
+            UIBarButtonItem(image: UIImage(systemName: "keyboard"), primaryAction: UIAction { [weak self] _ in
+                guard let `self` = self else { return }
+                if self.textView.inputView != nil {
+                    self.textView.inputView = nil
+                } else {
+                    self.textView.inputView = self.keyboard
+                }
+                self.textView.reloadInputViews()
+            }, menu: nil)
+        ]
+        
     }
     
     func setupScrollView() {
