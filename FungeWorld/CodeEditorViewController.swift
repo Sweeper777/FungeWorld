87f13081c16125cb6bcf998d6c537593ed1cd2e3
Mulang Su <sumulang@gmail.com>
2022-01-27 22:11:23 +0000

    implemented CodeEditorVC.doneDidTap

--- FungeWorld/CodeEditorViewController.swift
+++ FungeWorld/CodeEditorViewController.swift
@@ -7,11 +7,14 @@ class CodeEditorViewController: UIViewController {
     
     weak var delegate: CodeEditorViewControllerDelegate?
     
+    @IBOutlet var codeEditor: CodeEditorView!
     override func viewDidLoad() {
+        codeEditor.code = code
     }
     
     @IBAction func doneDidTap() {
-        
+        delegate?.didFinishEditingCode(code: codeEditor.code ?? "")
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func cancelDidTap() {
