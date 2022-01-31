847c2f308576a62a50f2e131ec592d98e38685d9
Mulang Su <sumulang@gmail.com>
2022-01-31 22:05:32 +0000

    Create WorldState.swift

---
+++ FungeWorld/WorldState.swift
@@ -0,0 +1,20 @@
+import Befunge
+
+@MainActor
+final class WorldState {
+    enum PlayingState: Codable {
+        case paused
+        case playing
+        case terminated
+    }
+    
+    var state: State
+    var code = ""
+    var playingState: PlayingState = .paused
+    var outputBuffer = [UnicodeScalar]()
+    var inputCharBuffer = [UnicodeScalar]()
+    
+    init(state: State) {
+        self.state = state
+    }
+}
